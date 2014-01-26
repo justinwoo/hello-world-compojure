73c3b8cb4d40dfdd4e6e17910a0e9d4505c7d649
Justin Woo <moomoowoo@gmail.com>
2014-01-25 21:27:37 -0600

    additional work
    works according to expected behavior

--- src/hello_world/handler.clj
+++ src/hello_world/handler.clj
@@ -8,17 +8,42 @@
   (GET "/" [] "Hello World")
   (GET "/user/:id"
   	[id]
-  	(@data-store 
-  		(keyword id)))
+    (let [id-key (keyword id)]
+      (if (contains? @data-store id-key)
+        (@data-store 
+          (keyword id))
+        (route/not-found
+          (str "User " id " was not found")))))
   (POST "/user/:id"
-  	[id]
-  	(str "we post"))
-  (PUT "/user/:id"
-  	[id]
-  	(str "we put"))
+  	[id]   
+    (let [id-key (keyword id)]
+      (if (contains? @data-store id-key)
+        {:status 400
+         :body "User " id "already exists -- consider PUT"}
+        (do
+          (swap! data-store 
+            assoc (keyword id) id)
+          (str "User " id " was added")))))
+  (PUT "/user/:id/:newvalue"
+  	[id newvalue]
+  	(let [id-key (keyword id)]
+      (if (contains? @data-store id-key)
+        (do
+          (swap! data-store 
+            assoc (keyword id) newvalue)
+          (str "User " id " was updated"))
+        {:status 400
+         :body "User " id "does not exist -- consider POST"})))
   (DELETE "/user/:id"
   	[id]
-  	(str "we delete"))
+    (let [id-key (keyword id)]
+      (if (contains? @data-store id-key)
+        (do
+          (swap! data-store 
+            dissoc (keyword id))
+          (str "User" id " was deleted"))
+        (route/not-found
+          (str "User " id " was not found")))))
   (route/resources "/")
   (route/not-found "Not Found"))
 
--- test/hello_world/test/handler.clj
+++ test/hello_world/test/handler.clj
@@ -18,6 +18,38 @@
     (let [response (app (request :get "/user/このユーザがあるわけがねえ"))]
       (is (= (:status response) 404))))
 
+  (testing "testing posting operation"
+    (let [response-1 (app (request :post "/user/2"))
+          response-2 (app (request :get "/user/2"))
+          response-3 (app (request :post "/user/1"))]
+      (is (= (:status response-1) 200))
+      (is (= (:status response-2) 200))
+      (is (= (:body response-2) "2"))
+      (is (= (:status response-3) 400))))
+
+  (testing "testing putting operation"
+    (let [response-1 (app (request :put "/user/1/2"))
+          response-2 (app (request :get "/user/1"))
+          response-3 (app (request :put "/user/3/3"))]
+          ; these subtests are not isolated: the state of 
+          ; the system is persisted throughout the test
+      (is (= (:status response-1) 200))
+      (is (= (:status response-2) 200))
+      (is (= (:body response-2) "2"))
+      (is (= (:status response-3) 400))))
+
+  (testing "testing deletion operation"
+    (let [response-1 (app (request :delete "/user/1"))
+          response-2 (app (request :get "/user/1"))]
+      (is (= (:status response-1) 200))
+      (is (= (:status response-2) 404))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
+(deftest newtest
+  ; this test is isolated from the previous test
+  (let [response (app (request :get "/user/1"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "1"))))
